28603a20043aeb3fbe0ae6a1a68c8b2e237b3848
AshanTharindu <atharindu94@gmail.com>
2018-11-09 16:05:03 +0530

    key generating line added

--- app/src/main/java/com/example/madhushika/carbc_android_v3/LaunchingEmptyActivity.java
+++ app/src/main/java/com/example/madhushika/carbc_android_v3/LaunchingEmptyActivity.java
@@ -12,6 +12,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import Objects.ReminderItem;
+import chainUtil.KeyGenerator;
 import controller.Controller;
 import network.communicationHandler.MessageSender;
 //import controller.Controller;
@@ -26,6 +27,7 @@ public class LaunchingEmptyActivity extends AppCompatActivity {
         Controller controller = new Controller();
         controller.startNode();
         MessageSender.requestIP();
+        KeyGenerator.getInstance().generateKeyPair();
 
         SharedPreferences preferences = getSharedPreferences("com.example.madhushika.carbc_android_v2", 0);
         boolean login_statusStored = preferences.getBoolean("login_status", false);
